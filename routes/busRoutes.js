ddd59c17eb83da38893f6dd5434867511ed938f0
Sparrow535 <rohanrai4133@gmail.com>
2024-09-11 15:56:22 +0600

    architecture

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const busController = require("../controllers/busController");
 
-router.post("/join", busController.joinBus);
 router.post("/:id/location", busController.updateLocation);
 router.get("/", busController.getBuses);
 
